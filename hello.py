97dc87a43c79b86360cc7beecf35183edd44a265
clipper598 <lvazquezcarlos@gmail.com>
2025-09-19 10:23:14 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -95,7 +95,7 @@ def hello():
         
     cursor = conexion_MySQLdb.cursor()
     try:
-        cursor.execute("SELECT id, barcode, product_name, quantity, price FROM products")
+        cursor.execute("SELECT id, barcode, product_name, quantity, price FROM products order by product_name")
         products = cursor.fetchall()
         print(products)
     finally:
@@ -124,7 +124,7 @@ def eliminaInventario():
 
             cursor = conexion_MySQLdb.cursor()
             try:
-                cursor.execute("SELECT id, barcode, product_name, quantity, price FROM products")
+                cursor.execute("SELECT id, barcode, product_name, quantity, price FROM products order by product_name")
                 products = cursor.fetchall()
                 #print(products)
                 conexion_MySQLdb.commit()
